d6d9637a7c4dac54be6dfbb14ad28f837f9c228a
Sergey Maslennikov <maslennikovserge@yandex.ru>
2021-05-13 18:48:48 +0300

    Add: GNSS MTK Transystem EB-800A

--- Gnss/libConfig.h
+++ Gnss/libConfig.h
@@ -3,5 +3,5 @@
 #define LIB_UTILS_LOG
 #define LIB_UTILS_LOG_COLOUR
 
-#define LIB_MOD_GNSS_LatitudeSizeFract  4
-#define LIB_MOD_GNSS_LongitudeSizeFract 4
+#define LIB_MOD_GNSS_LatitudeSizeFract  6
+#define LIB_MOD_GNSS_LongitudeSizeFract 6
